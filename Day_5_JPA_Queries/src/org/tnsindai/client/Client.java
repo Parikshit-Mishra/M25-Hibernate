b3dfb14cc093de32d16700d74ed0588d8529134e
Namrata Mishra <namratapande111@gmail.com>
2022-11-25 16:49:44 +0530

    commit 2

--- Day_5_JPA_Queries/src/org/tnsindai/client/Client.java
+++ Day_5_JPA_Queries/src/org/tnsindai/client/Client.java
@@ -16,7 +16,7 @@ public class Client {
 		int id=s.nextInt();
 		System.out.println("Book with ID "+id+":"+service.getBookById(id));
 		
-		System.out.println("Listing book with Specific Title: ");
+		/*System.out.println("Listing book with Specific Title: ");
 		System.out.println("Enter the title:");
 		String title=s.next();
 		System.out.println("Book with Specific "+title+":"+service.getBookByTitle(title));
@@ -28,11 +28,10 @@ public class Client {
 		
 		System.out.println("Listing book with Specific price range: ");
 		for(Book book:service.getBookInPriceRange(300.0, 600.0))
-			System.out.println(book);
+			System.out.println(book);*/	
 		
-		
-		System.out.println("Listing all the books: ");
-		System.out.println(service.getAllBooks());
+		//System.out.println("Listing all the books: ");
+		//System.out.println(service.getAllBooks());
 	}
 
 }
